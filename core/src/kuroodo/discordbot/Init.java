43b31537eeb1125923d5ff68d34aa5e81a025ff5
kuroodo <kuroododitory@gmail.com>
2016-12-05 17:46:23 -0500

    1.9.5 release

--- core/src/kuroodo/discordbot/Init.java
+++ core/src/kuroodo/discordbot/Init.java
@@ -27,7 +27,7 @@ import net.dv8tion.jda.core.exceptions.RateLimitedException;
 
 public class Init extends ApplicationAdapter {
 
-	public static final String SUPERUSER_PASSWORD = JSonReader.getSuperUserPassword(), VERSION = "1.9.4";
+	public static final String SUPERUSER_PASSWORD = JSonReader.getSuperUserPassword(), VERSION = "1.9.5";
 	// Users that have special bot commands and access
 	public static final ArrayList<User> SUPER_USERS = new ArrayList<>();
 
