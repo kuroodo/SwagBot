88251f8955a14b0c45d7ef09671013742a9b908a
kuroodo <kuroododitory@gmail.com>
2017-04-30 17:04:08 -0400

    Edit CommandHelp audio information

--- core/src/kuroodo/discordbot/chatcommands/CommandHelp.java
+++ core/src/kuroodo/discordbot/chatcommands/CommandHelp.java
@@ -1,9 +1,5 @@
 package kuroodo.discordbot.chatcommands;
 
-import java.io.File;
-import java.io.FilenameFilter;
-import java.util.ArrayList;
-
 import kuroodo.discordbot.Init;
 import kuroodo.discordbot.client.handlers.ChatCommandHandler;
 import kuroodo.discordbot.entities.ChatCommand;
@@ -113,13 +109,14 @@ public class CommandHelp extends ChatCommand {
 
 	private void printHiddenCommands(String commandWord) {
 		switch (commandWord) {
+		case "!audioplayer":
 		case "!play":
 		case "!playrandom":
 		case "!stop":
 		case "!skip":
 		case "!pause":
 		case "!resume":
-		case "trackinfo":
+		case "!trackinfo":
 			sendPrivateMessage("A hidden command, these commands are for using the audio player\n\n"
 					+ "!play [filelocation/url] makes the bot play a audio from a specified source"
 					+ "Example: !play https://someurl.com/audiofile.mp3 OR !play c:\\somefilelocation\\audiofile.mp3\n"
@@ -132,60 +129,60 @@ public class CommandHelp extends ChatCommand {
 
 			break;
 		case "!soundboard":
-			ArrayList<String> soundList = new ArrayList<>();
-			String sounds = "There are currently no sounds available";
-			String message = "A hidden command, these commands are for playing special sounds with the audioplayer."
-					+ " (see more: !help play)" + "\nUsage: !soundname (example !bradberry)\n" + "List of sounds\n\n";
+//			ArrayList<String> soundList = new ArrayList<>();
+//			String sounds = "There are currently no sounds available";
+//			String message = "A hidden command, these commands are for playing special sounds with the audioplayer."
+//					+ " (see more: !help play)" + "\nUsage: !soundname (example !bradberry)\n" + "List of sounds\n\n";
+//
+//			FilenameFilter mp3Filter = new FilenameFilter() {
+//				@Override
+//				public boolean accept(File dir, String name) {
+//					if (name.endsWith(".mp3")) {
+//						// filters files whose extension is .mp3
+//						return true;
+//					} else {
+//						return false;
+//					}
+//				}
+//			};
+//
+//			String filePath = "sounds/";
+//			File dir = new File(filePath);
+//			File[] files = dir.listFiles(mp3Filter);
+//
+//			if (files != null) {
+//				if (files.length == 0) {
+//					System.out.println("No MP3 files found");
+//
+//				} else {
+//					sounds = "";
+//					int stringLength = 0;
+//					// Limit message to around 1700 characters
+//					int characterLimit = 1700;
+//					for (File file : files) {
+//						// Get the name of the sound file without .mp3 extension
+//						sounds = sounds + "!" + file.getName().toLowerCase().replaceFirst("[.][^.]+$", "") + " ";
+//						stringLength = sounds.length();
+//
+//						if (stringLength >= characterLimit) {
+//							System.out.println("Debug: soundboard string size" + stringLength);
+//							soundList.add(sounds);
+//							sounds = "";
+//						}
+//
+//					}
+//
+//					if (!soundList.isEmpty()) {
+//						sendPrivateMessage(message);
+//						for (String soundString : soundList) {
+//							sendPrivateMessage(soundString);
+//						}
+//						break;
+//					}
+//				}
+//			}
 
-			FilenameFilter mp3Filter = new FilenameFilter() {
-				@Override
-				public boolean accept(File dir, String name) {
-					if (name.endsWith(".mp3")) {
-						// filters files whose extension is .mp3
-						return true;
-					} else {
-						return false;
-					}
-				}
-			};
-
-			String filePath = "sounds/";
-			File dir = new File(filePath);
-			File[] files = dir.listFiles(mp3Filter);
-
-			if (files != null) {
-				if (files.length == 0) {
-					System.out.println("No MP3 files found");
-
-				} else {
-					sounds = "";
-					int stringLength = 0;
-					// Limit message to around 1700 characters
-					int characterLimit = 1700;
-					for (File file : files) {
-						// Get the name of the sound file without .mp3 extension
-						sounds = sounds + "!" + file.getName().toLowerCase().replaceFirst("[.][^.]+$", "") + " ";
-						stringLength = sounds.length();
-
-						if (stringLength >= characterLimit) {
-							System.out.println("Debug: soundboard string size" + stringLength);
-							soundList.add(sounds);
-							sounds = "";
-						}
-
-					}
-
-					if (!soundList.isEmpty()) {
-						sendPrivateMessage(message);
-						for (String soundString : soundList) {
-							sendPrivateMessage(soundString);
-						}
-						break;
-					}
-				}
-			}
-
-			sendPrivateMessage(message + sounds);
+			sendPrivateMessage("Please use LitBot for the soundboard. Or perhaps you meant AudioPlayer?");
 			break;
 		default:
 			break;
